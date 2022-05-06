6c60c650bd7d36d421ca16426198134c17283c25
Marian <kalbasa@example.com>
2022-05-06 18:31:01 +0200

    one

--- 3/js/main.js
+++ 3/js/main.js
@@ -2,12 +2,23 @@ const html = await fetch('./editView.html').then(response => response.text());
 
 editBtn.addEventListener("click", editView);
 
+function recurse(times,func){
+    if (times==0){
+        return ()=>requestAnimationFrame(func);
+    }
+    const f=recurse(--times,func);
+    return ()=>requestAnimationFrame(f);
+}
+
 async function editView() {
     rootDiv.innerHTML = html;
     requestAnimationFrame(() => {
         textArea.style.height = textArea.scrollHeight + "px";
-        requestAnimationFrame(() =>
-            requestAnimationFrame(() =>
-                requestAnimationFrame(() => saveBtn.addEventListener("click", () => console.log(">save")))));
+        let f = () => {
+            console.log("added");
+            saveBtn.addEventListener("click", () => console.log(">save"));
+        };
+        recurse(10,f)();
+        
     });
 }
