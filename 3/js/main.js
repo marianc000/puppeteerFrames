e6a2b5a3d6ce02aed7351c3f32a74a67f96ddd2c
Marian <kalbasa@example.com>
2022-05-06 22:02:16 +0200

    one

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,24 +1,31 @@
 const html = await fetch('./editView.html').then(response => response.text());
 
+
+
+const params = new URLSearchParams(window.location.search);
+const frames = parseInt(params.get('frames') ?? 10);
+console.log("frames", frames);
+
 editBtn.addEventListener("click", editView);
 
-function recurse(times,func){
-    if (times==0){
-        return ()=>requestAnimationFrame(func);
+function recurse(times, func) {
+    if (times === 0) {
+        return () => requestAnimationFrame(func);
     }
-    const f=recurse(--times,func);
-    return ()=>requestAnimationFrame(f);
+    const f = recurse(--times, func);
+    return () => requestAnimationFrame(f);
 }
 
 async function editView() {
     rootDiv.innerHTML = html;
     requestAnimationFrame(() => {
-        textArea.style.height = textArea.scrollHeight + "px";
+        textArea.style.height = textArea.scrollHeight + "px"; 
+        const t0 = Date.now();
         let f = () => {
-            console.log("added");
-            saveBtn.addEventListener("click", () => console.log(">save"));
+            saveBtn.addEventListener("click", () => console.log(">save")); 
+            console.log((Date.now() - t0) / 1000);
         };
-        recurse(10,f)();
-        
+       
+        recurse(frames, f)();
     });
 }
