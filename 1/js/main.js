e20729fd7caa330c71f5165a2332a1183b2e6e9c
Marian <kalbasa@example.com>
2022-05-06 14:33:57 +0200

    one

--- 1/js/main.js
+++ 1/js/main.js
@@ -1,11 +1,10 @@
-const html=await fetch( './editView.html' ).then(response => response.text());
-    
+const html = await fetch('./editView.html').then(response => response.text());
+
 editBtn.addEventListener("click", editView);
 
-async function editView(){
+async function editView() {
     console.log(">editView");
-    rootDiv.innerHTML=html;
-    requestAnimationFrame(()=>textArea.style.height=textArea.scrollHeight + "px");
+    rootDiv.innerHTML = html;
+    requestAnimationFrame(() => textArea.style.height = textArea.scrollHeight + "px");
+    saveBtn.addEventListener("click", () => console.log(">save"));
 }
-
-saveBtn.addEventListener("click", ()=>console.log(">save"));
