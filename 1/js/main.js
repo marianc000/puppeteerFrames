a30c3d8cc42035e4f930c8df5bf2daf2b4ff148c
Marian <kalbasa@example.com>
2022-05-06 16:27:07 +0200

    one

--- 1/js/main.js
+++ 1/js/main.js
@@ -5,6 +5,9 @@ editBtn.addEventListener("click", editView);
 async function editView() {
     console.log(">editView");
     rootDiv.innerHTML = html;
-    requestAnimationFrame(() => textArea.style.height = textArea.scrollHeight + "px");
-    saveBtn.addEventListener("click", () => console.log(">save"));
+    requestAnimationFrame(() => {
+        textArea.style.height = textArea.scrollHeight + "px";
+        saveBtn.addEventListener("click", () => console.log(">save"));
+    });
+
 }
--- 1/puppeteer1.js
+++ 1/puppeteer1.js
@@ -1,16 +1,17 @@
 import puppeteer from 'puppeteer';
 
 const browser = await puppeteer.launch({
- // headless: false,
+  // headless: false,
   args: ['--start-maximized'],
 });
 
 let page = await browser.newPage();
 page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-await page.goto("https://puppeteerframes.onrender.com/1/index.html",{waitUntil:'networkidle0'});
+await page.goto("https://puppeteerframes.onrender.com/1/index.html", { waitUntil: 'networkidle0' });
 //await page.goto("http://192.168.0.24:5500/1/index.html");
 //await page.click("#editBtn");
+await page.screenshot({ path: '0.png' });
 await page.evaluate(() => editBtn.click());
+await page.screenshot({ path: '1.png' });
 await page.evaluate(() => saveBtn.click());
-//await page.screenshot({ path: '1.png' });
- await browser.close();
+await browser.close();
