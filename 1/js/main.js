3cecc4ddd5b62b8fe2e99c79727dff12485adec8
Marian <kalbasa@example.com>
2022-05-06 17:03:17 +0200

    one

--- 1/js/main.js
+++ 1/js/main.js
@@ -3,7 +3,6 @@ const html = await fetch('./editView.html').then(response => response.text());
 editBtn.addEventListener("click", editView);
 
 async function editView() {
-    console.log(">editView");
     rootDiv.innerHTML = html;
     requestAnimationFrame(() => {
         textArea.style.height = textArea.scrollHeight + "px";
--- 1/puppeteer1.js
+++ 1/puppeteer1.js
@@ -11,7 +11,9 @@ await page.goto("https://puppeteerframes.onrender.com/1/index.html", { waitUntil
 //await page.goto("http://192.168.0.24:5500/1/index.html");
 //await page.click("#editBtn");
 await page.screenshot({ path: '0.png' });
-await page.evaluate(() => editBtn.click());
-await page.evaluate(() => saveBtn.click());
-await page.screenshot({ path: '1.png' });
+ await page.evaluate(() => editBtn.click());
+// await page.click("#editBtn"); 
+// await page.click("#saveBtn");
+ await page.evaluate(() => saveBtn.click());
+await page.screenshot({ path: '2.png' });
 await browser.close();
